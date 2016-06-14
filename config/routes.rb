4b98ffe5a5dc67b269d353058007aa0cd2640b80
Alvaro Javier <a20130527@utem.edu.mx>
2016-06-14 13:56:35 -0500

    ajustes varios, no afectan el sistema en si

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  get 'welcome/Ayuda'
+  get 'welcome/ayuda'
   get 'welcome/acerca_de'
 
   # The priority is based upon order of creation: first created -> highest priority.
